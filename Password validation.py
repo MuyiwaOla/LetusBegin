182a371227de4852531f9216b6b2804f89482401
MuyiwaOla <58740944+MuyiwaOla@users.noreply.github.com>
2020-01-04 20:30:32 +0000

    Add files via upload
    This file contains my trial attempt at the password validation task I will still be working on this one in my own time.

---
+++ Password validation.py
@@ -0,0 +1,11 @@
+import re
+print('This is a password checker')
+password = str(input('Please enter your password: '))
+
+checker = re.search('[a-z]','[A-Z]',password)
+
+if (checker):
+    print('Your password passes the checks')
+else:
+    print('Make a new password')
+    
