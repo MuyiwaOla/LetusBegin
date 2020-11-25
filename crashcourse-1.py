b4299f62ab90cc8e0b959c6a17b5c850cce7c961
Muyiwa Olayisade <muyiwaaws@gmail.com>
2020-11-25 16:09:23 +0000

    crash course work

---
+++ crashcourse-1.py
@@ -0,0 +1,18 @@
+#Test 1
+
+# car = 'audi'
+# print("Is car == 'subaru'? I predict True.")
+# print(car == 'subaru')
+
+# print("\nIs car == 'audi'? I predict False.")
+# print(car == 'audi')
+
+#Test 2
+munch = 'Kokoro'
+input ("what are you eating")
+
+if input == munch :
+    print('Stop spending')
+else:
+    print('go sort out your stomach')
+
