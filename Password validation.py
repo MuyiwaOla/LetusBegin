1b7d0bf3d68f318de7b8be7be83bdc0fc661a834
Muyiwa Olayisade <muyiwaaws@gmail.com>
2020-11-27 10:42:48 +0000

    fix

--- Password validation.py
+++ Password validation.py
@@ -2,7 +2,7 @@ import re
 print('This is a password checker')
 password = str(input('Please enter your password: '))
 
-checker = re.search('[a-z]','[A-Z]',password)
+checker = re.search('[a-z]','[A-Z]', password)
 
 if (checker):
     print('Your password passes the checks')
